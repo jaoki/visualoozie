bb398c5ded0a4a47c938863837fa843cfe8951cb
jun aoki <jun.aoki.0713@gmail.com>
2013-02-17 13:57:43 -0800

    added shell03 xsd:

--- src/main/java/visualoozie/util/XmlLoader.java
+++ src/main/java/visualoozie/util/XmlLoader.java
@@ -1,18 +1,21 @@
 package visualoozie.util;
 
 import java.io.ByteArrayInputStream;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
 import org.xml.sax.SAXException;
 
+import visualoozie.xsd.shell03.ACTION;
 import visualoozie.xsd.workflow04.WORKFLOWAPP;
 
 public class XmlLoader {
@@ -23,17 +26,22 @@ public class XmlLoader {
 //            JAXBContext context = JAXBContext.newInstance("visualoozie.xsd");
             JAXBContext context = JAXBContext.newInstance(
         		WORKFLOWAPP.class
+        		, ACTION.class
     		);
             unmarshaller = context.createUnmarshaller();
         } catch (JAXBException e) {
             e.printStackTrace();
         }
 
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        ClassLoader classLoader = XmlLoader.class.getClassLoader();
+        List<StreamSource> sources = new ArrayList<StreamSource>();
+
+        sources.add(new StreamSource(classLoader.getResourceAsStream("oozie/oozie-workflow-0.4.xsd")));
+        sources.add(new StreamSource(classLoader.getResourceAsStream("oozie/shell-action-0.3.xsd")));
+
         try {
-            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            ClassLoader classLoader = XmlLoader.class.getClassLoader();
-            URL url = classLoader.getResource("oozie/oozie-workflow-0.4.xsd");
-            Schema schema = sf.newSchema(url);
+        	Schema schema = sf.newSchema(sources.toArray(new StreamSource[0]));
             unmarshaller.setSchema(schema);
         } catch (SAXException e) {
             e.printStackTrace();
